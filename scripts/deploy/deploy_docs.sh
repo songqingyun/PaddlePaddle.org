87ad7e4f702835c015ef8a7497f10cd412749efd
Varun Arora <aroravarun@baidu.com>
2018-11-20 12:00:13 -0800

    Actually don't need to re-export these guys

--- scripts/deploy/deploy_docs.sh
+++ scripts/deploy/deploy_docs.sh
@@ -60,9 +60,6 @@ rsync -r /var/pages/menus/ content_mgr@$STAGE_DEPLOY_IP:/var/pages/menus
 rsync -r /var/pages/indexes/ content_mgr@$STAGE_DEPLOY_IP:/var/pages/indexes
 
 echo "7. Deploy indexes."
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-
 aws s3 cp /var/pages/indexes/indexes s3://paddlepaddle.org/indexes  --recursive
 
 echo "8. Documentation deployed. Clean up."
