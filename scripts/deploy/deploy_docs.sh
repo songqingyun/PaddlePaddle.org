d70cda87a479481991cb551d732df7620e369301
Varun Arora <aroravarun@baidu.com>
2018-11-20 13:55:24 -0800

    Gzip and set public ACL

--- scripts/deploy/deploy_docs.sh
+++ scripts/deploy/deploy_docs.sh
@@ -60,7 +60,8 @@ rsync -r /var/pages/menus/ content_mgr@$STAGE_DEPLOY_IP:/var/pages/menus
 rsync -r /var/pages/indexes/ content_mgr@$STAGE_DEPLOY_IP:/var/pages/indexes
 
 echo "7. Deploy indexes."
-aws s3 cp /var/pages/indexes/indexes s3://paddlepaddle.org/indexes  --recursive
+gzip -r /var/pages/indexes/
+aws s3 cp /var/pages/indexes/indexes s3://paddlepaddle.org/indexes  --recursive --acl public-read
 
 echo "8. Documentation deployed. Clean up."
 chmod 644 content_mgr.pem
